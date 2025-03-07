59c4098365511a055f29d50307eb5d31f31ead28
Dan Milkie <dmilkie@gmail.com>
2025-03-07 13:14:45 -0500

    Update data_utils.py rename to batch_config

--- data/data_utils.py
+++ data/data_utils.py
@@ -3,13 +3,13 @@ from itertools import product
 from .data_config import DataConfig, ColorMode
 
 def index_mapper(shape: tuple[int, int, int, int, int ,int],
-                 data_config : DataConfig) -> list[tuple[int, int, int, int, int, int]]:
+                 batch_config : DataConfig) -> list[tuple[int, int, int, int, int, int]]:
     """
     Given a tensorstore object shape and data config which contains batch shape information, returns a list of tuples
     that map an index to
     Args:
         shape: Tensorstore object shape
-        data_config: DataConfig object which contains batch shape information and how to handle color channels
+        batch_config: DataConfig object which contains batch shape information and how to handle color channels
 
     Returns:
         indices: list of indices that map a batch index to the tile index and {time,z,y,x,c} slices
@@ -18,31 +18,31 @@ def index_mapper(shape: tuple[int, int, int, int, int ,int],
     n_tile, n_time, n_z, n_y, n_x, n_c = shape
 
     # Calculate the number of batches in each store
-    if data_config.color_mode == ColorMode.MATCH and n_c != data_config.c:
+    if batch_config.color_mode == ColorMode.MATCH and n_c != batch_config.c:
         return None
 
-    if data_config.color_mode == ColorMode.AVG or  data_config.color_mode == ColorMode.MATCH:
+    if batch_config.color_mode == ColorMode.AVG or  batch_config.color_mode == ColorMode.MATCH:
         # AVG: output will be averaged so will have a single color channel
         # MATCH: output channel size must match input channel size, therefore color channel won't be s
         n_c = 1
     else:
-        raise NotImplementedError(f"color mode {data_config.color_mode} is not supported")
+        raise NotImplementedError(f"color mode {batch_config.color_mode} is not supported")
 
-    n_time = n_time // data_config.t
-    n_z = n_z // data_config.z
-    n_y = n_y // data_config.y
-    n_x = n_x // data_config.x
+    n_time = n_time // batch_config.t
+    n_z = n_z // batch_config.z
+    n_y = n_y // batch_config.y
+    n_x = n_x // batch_config.x
 
     indices = list(product(range(n_tile), range(n_time), range(n_z), range(n_y), range(n_x), range(n_c)))
 
     return indices
 
-def middle_out_crop_start_index(shape: tuple[int, int, int, int, int ,int], data_config : DataConfig) -> tuple[int, int]:
+def middle_out_crop_start_index(shape: tuple[int, int, int, int, int ,int], batch_config : DataConfig) -> tuple[int, int]:
     """
     Due to increased optical performance on axis, data should be cropped from middle out
     Args:
         shape: Tensorstore object shape
-        data_config: DataConfig object which contains batch shape information and how to handle color channels
+        batch_config: DataConfig object which contains batch shape information and how to handle color channels
 
     Returns:
         (x0, y0): Pixel offset to achieve middle out crop
@@ -50,7 +50,7 @@ def middle_out_crop_start_index(shape: tuple[int, int, int, int, int ,int], data
     # Tensorstore object dimensions assumed to be in (N,T,Z,Y,X,C) format
     n_tile, n_time, n_z, n_y, n_x, n_c = shape
 
-    y0 = (n_y % data_config.y) // 2
-    x0 = (n_x % data_config.x) // 2
+    y0 = (n_y % batch_config.y) // 2
+    x0 = (n_x % batch_config.x) // 2
 
     return y0, x0
