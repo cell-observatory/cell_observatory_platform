96ca2a9f6b40e141dca66b9fa182566125e4ed33
thayer <thayeralshaabi@gmail.com>
2025-05-01 15:05:24 -0400

    new model templates

--- models/vit.py
+++ models/vit.py
@@ -52,6 +52,18 @@ CONFIGS = {
         'num_heads': 16,
         'mlp_ratio': 4,
     },
+    'vit-2billion': {
+        'embed_dim': 2560,
+        'depth': 24,
+        'num_heads': 32,
+        'mlp_ratio': 4,
+    },
+    'vit-6billion': {
+        'embed_dim': 4096,
+        'depth': 32,
+        'num_heads': 32,
+        'mlp_ratio': 4,
+    },
     'vit-giant': {
         'embed_dim': 1408,
         'depth': 40,
@@ -63,6 +75,12 @@ CONFIGS = {
         'depth': 48,
         'num_heads': 16,
         'mlp_ratio': 64/13,
+    },
+    'vit-enormous': {
+        'embed_dim': 1792,
+        'depth': 56,
+        'num_heads': 16,
+        'mlp_ratio': 8.5714285714,
     }
 }
 
