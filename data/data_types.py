a7324bbe740571665a287e7cd0e96f0cb6b8cef8
thayer <thayeralshaabi@gmail.com>
2025-06-05 16:05:33 -0400

    combine tests

--- data/data_types.py
+++ data/data_types.py
@@ -1,6 +1,15 @@
+import sys
+import logging
 import torch
 import numpy as np
 from enum import Enum
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
 
 class TORCH_DTYPES(Enum):
     float32 = torch.float32
