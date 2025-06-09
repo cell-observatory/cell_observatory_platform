608734976f8a00523f147772ae9533bdecd41eab
thayer <thayeralshaabi@gmail.com>
2025-06-09 13:49:07 -0400

    more utils for hypercube enums

--- data/data_shapes.py
+++ data/data_shapes.py
@@ -34,6 +34,15 @@ class MULTICHANNEL_3D_HYPERCUBE(Enum):
     def axes(self) -> Tuple[str, ...]:
         return tuple(self.value)  # e.g. ("C","Z","Y","X")
 
+    def is_channel_first(self) -> bool:
+        return self in (MULTICHANNEL_3D_HYPERCUBE.CZYX, MULTICHANNEL_3D_HYPERCUBE.CTYX)
+
+    def is_channel_last(self) -> bool:
+        return self in (MULTICHANNEL_3D_HYPERCUBE.ZYXC, MULTICHANNEL_3D_HYPERCUBE.TYXC)
+
+    def has_temporal_dim(self) -> bool:
+        return self in (MULTICHANNEL_3D_HYPERCUBE.CTYX, MULTICHANNEL_3D_HYPERCUBE.TYXC)
+
     def get_image_shape_tuple(self, tensor: torch.Tensor) -> Tuple:
         has_batch = tensor.ndim == 5  # (N, Z/T, Y, X, C)
         shape = tensor.shape #eg (512, 128, 128, 128, 2)
@@ -52,16 +61,18 @@ class MULTICHANNEL_3D_HYPERCUBE(Enum):
         shape = tensor.shape #eg (512, 128, 128, 128, 2)
 
         if self is MULTICHANNEL_3D_HYPERCUBE.ZYXC:
-            return dict(z=shape[1], y=shape[2], x=shape[3]) if has_batch else dict(z=shape[0], y=shape[1], x=shape[2])
+            return dict(z=shape[1], y=shape[2], x=shape[3], c=shape[4]) \
+                if has_batch else dict(z=shape[0], y=shape[1], x=shape[2], c=shape[3])
 
         elif self is MULTICHANNEL_3D_HYPERCUBE.TYXC:
-            return dict(t=shape[1], y=shape[2], x=shape[3]) if has_batch else dict(t=shape[0], y=shape[1], x=shape[2])
+            return dict(t=shape[1], y=shape[2], x=shape[3], c=shape[4]) \
+                if has_batch else dict(t=shape[0], y=shape[1], x=shape[2], c=shape[3])
 
         elif self is MULTICHANNEL_3D_HYPERCUBE.CZYX:
-            return dict(z=shape[-3], y=shape[-2], x=shape[-1])
+            return dict(c=shape[-4], z=shape[-3], y=shape[-2], x=shape[-1])
 
         elif self is MULTICHANNEL_3D_HYPERCUBE.CTYX:
-            return dict(t=shape[-3], y=shape[-2], x=shape[-1])
+            return dict(c=shape[-4], t=shape[-3], y=shape[-2], x=shape[-1])
         else:
             raise NotImplementedError(f"Unsupported layout {shape}")
 
@@ -100,6 +111,14 @@ class MULTICHANNEL_3D_HYPERCUBE(Enum):
         perm = (0, 2, 3, 4, 1) if has_batch else (1, 2, 3, 0)
         return tensor.permute(*perm)
 
+    def num_channels(self, tensor: torch.Tensor) -> int:
+        d = self.get_image_shape_dict(tensor)
+        return d['c']
+
+    def num_timepoints(self, tensor: torch.Tensor) -> int | None:
+        d = self.get_image_shape_dict(tensor)
+        return d.get('t', None)
+
 
 class MULTICHANNEL_4D_HYPERCUBE(Enum):
     """Spatiotemporal 4D multichannel hypercube layouts.
@@ -118,7 +137,17 @@ class MULTICHANNEL_4D_HYPERCUBE(Enum):
     def axes(self) -> Tuple[str, ...]:
         return tuple(self.value)  # e.g. ("C","T","Z","Y","X")
 
+    def is_channel_first(self) -> bool:
+        return self is MULTICHANNEL_4D_HYPERCUBE.CTZYX
+
+    def is_channel_last(self) -> bool:
+        return self is MULTICHANNEL_4D_HYPERCUBE.TZYXC
+
+    def has_temporal_dim(self) -> bool:
+        return self is MULTICHANNEL_4D_HYPERCUBE.CTZYX
+
     def get_image_shape_tuple(self, tensor: torch.Tensor) -> Tuple:
+
         has_batch = tensor.ndim == 6  # (N, T, Z, Y, X, C)
         shape = tensor.shape #eg (512, 16, 128, 128, 128, 2)
 
@@ -136,11 +165,11 @@ class MULTICHANNEL_4D_HYPERCUBE(Enum):
         shape = tensor.shape  # eg (512, 16, 128, 128, 128, 2)
 
         if self is MULTICHANNEL_4D_HYPERCUBE.TZYXC:
-            return dict(t=shape[1], z=shape[2], y=shape[3], x=shape[4]) \
-                if has_batch else dict(t=shape[0], z=shape[1], y=shape[2], x=shape[3])
+            return dict(t=shape[1], z=shape[2], y=shape[3], x=shape[4], c=shape[5]) \
+                if has_batch else dict(t=shape[0], z=shape[1], y=shape[2], x=shape[3], c=shape[4])
 
         elif self is MULTICHANNEL_4D_HYPERCUBE.CTZYX:
-            return dict(t=shape[-4], z=shape[-3], y=shape[-2], x=shape[-1])
+            return dict(c=shape[-5], t=shape[-4], z=shape[-3], y=shape[-2], x=shape[-1])
 
         else:
             raise NotImplementedError(f"Unsupported layout {shape}")
@@ -168,3 +197,11 @@ class MULTICHANNEL_4D_HYPERCUBE(Enum):
         has_batch = tensor.ndim == 6  # (N, C, T, Z, Y, X)
         perm = (0, 2, 3, 4, 5, 1) if has_batch else (1, 2, 3, 4, 0)
         return tensor.permute(*perm)
+
+    def num_channels(self, tensor: torch.Tensor) -> int:
+        d = self.get_image_shape_dict(tensor)
+        return d['c']
+
+    def num_timepoints(self, tensor: torch.Tensor) -> int | None:
+        d = self.get_image_shape_dict(tensor)
+        return d.get('t', None)
