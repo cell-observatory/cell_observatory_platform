568074c9e65d5d6d4e9fd6c10dcc2ca3e22a266e
thayer <thayeralshaabi@gmail.com>
2025-06-11 09:53:04 -0400

    add support for lsf and slurm

--- cluster/ray_start_worker.sh
+++ cluster/ray_start_worker.sh
@@ -6,23 +6,23 @@ export NCCL_P2P_LEVEL=NVL
 
 while getopts ":a:c:g:t:" option;do
     case "${option}" in
-    a) a=${OPTARG}
-       cluster_address=$a
-       echo cluster_address=$cluster_address
+    a)  a=${OPTARG}
+        cluster_address=$a
+        echo cluster_address=$cluster_address
     ;;
-    c) c=${OPTARG}
-       cpus=$c
-       echo cpus=$cpus
+    c)  c=${OPTARG}
+        cpus=$c
+        echo cpus=$cpus
     ;;
-    g) g=${OPTARG}
-       gpus=$g
-       echo gpus=$gpus
+    g)  g=${OPTARG}
+        gpus=$g
+        echo gpus=$gpus
     ;;
-    t) t=${OPTARG}
-       tmpdir=$t
-       echo tmpdir=$tmpdir
+    t)  t=${OPTARG}
+        tmpdir=$t
+        echo tmpdir=$tmpdir
     ;;
-    *) echo "Did not supply the correct arguments"
+    *)  echo "Did not supply the correct arguments"
     ;;
     esac
 done
@@ -32,5 +32,21 @@ job="ray start --address=$cluster_address --num-cpus=$cpus --num-gpus=$gpus --te
 echo $job
 $job &
 
-echo "Ray worker LSF ID: $LSB_JOBID"
+
+if command -v srun >/dev/null 2>&1; then
+    echo "SLURM is available on this cluster"
+    srun --version
+    scheduler="slurm"
+    echo "Ray worker SLURM ID: $SLURM_JOB_ID"
+
+elif command -v bsub >/dev/null 2>&1; then
+    echo "LSF is available on this cluster"
+    lsid
+    scheduler="lsf"
+else
+    echo "Neither SLURM nor LSF is available on this cluster"
+    scheduler="none"
+    echo "Ray worker LSF ID: $LSB_JOBID"
+fi
+
 sleep infinity
