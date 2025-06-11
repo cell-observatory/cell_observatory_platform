5bb960c42df9df912406122ba79618961e477a18
thayer <thayeralshaabi@gmail.com>
2025-06-11 10:08:34 -0400

    cleanup

--- cluster/ray_start_cluster.sh
+++ cluster/ray_start_cluster.sh
@@ -6,31 +6,31 @@ export NCCL_P2P_LEVEL=NVL
 
 while getopts ":i:p:d:c:g:t:" option;do
     case "${option}" in
-    i) i=${OPTARG}
-       ip=$i
-       echo ip=$ip
+    i)  i=${OPTARG}
+        ip=$i
+        echo ip=$ip
     ;;
-    p) p=${OPTARG}
-       port=$p
-       echo port=$port
+    p)  p=${OPTARG}
+        port=$p
+        echo port=$port
     ;;
-    d) d=${OPTARG}
-       dashboard_port=$d
-       echo dashboard_port=$dashboard_port
+    d)  d=${OPTARG}
+        dashboard_port=$d
+        echo dashboard_port=$dashboard_port
     ;;
-    c) c=${OPTARG}
-       cpus=$c
-       echo cpus=$cpus
+    c)  c=${OPTARG}
+        cpus=$c
+        echo cpus=$cpus
     ;;
-    g) g=${OPTARG}
-       gpus=$g
-       echo gpus=$gpus
+    g)  g=${OPTARG}
+        gpus=$g
+        echo gpus=$gpus
     ;;
-    t) t=${OPTARG}
-       tmpdir=$t
-       echo tmpdir=$tmpdir
+    t)  t=${OPTARG}
+        tmpdir=$t
+        echo tmpdir=$tmpdir
     ;;
-    *) echo "Did not supply the correct arguments"
+    *)  echo "Did not supply the correct arguments"
     ;;
     esac
 done
@@ -41,5 +41,5 @@ echo "Starting ray head node @ $(hostname) => $cluster_address with CPUs[$cpus] 
 job="ray start --head --node-ip-address=$ip --port=$port --dashboard-port=$dashboard_port --dashboard-host=0.0.0.0 --min-worker-port 18999 --max-worker-port 19999 --temp-dir=$tmpdir --num-cpus=$cpus --num-gpus=$gpus"
 echo $job
 $job &
-
+ray metrics launch-prometheus &
 sleep infinity
