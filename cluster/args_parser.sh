aa053e6bde183a141f8b7f7663c3456903036570
thayer <thayeralshaabi@gmail.com>
2025-06-13 18:19:29 -0400

    simplified arg parser for ray

---
+++ cluster/args_parser.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while getopts ":b:c:e:g:m:n:o:p:s:t:x:z:" option; do
+    case "${option}" in
+    b)  bind=${OPTARG} ;;
+    c)  cpus=${OPTARG} ;;
+    e)  env=${OPTARG} ;;
+    g)  gpus=${OPTARG} ;;
+    m)  mem=${OPTARG} ;;
+    n)  nodes=${OPTARG} ;;
+    o)  outdir=${OPTARG} ;;
+    p)  partition=${OPTARG} ;;
+    s)  workspace=${OPTARG} ;;
+    t)  tasks=${OPTARG} ;;
+    x)  exclusive=true ;;
+    z)  head_cpus=${OPTARG} ;;
+    *) echo "Did not supply the correct arguments"; exit 1 ;;
+    esac
+done
