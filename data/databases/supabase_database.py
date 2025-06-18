acb2d2a05edc5a2d19a96263eccc3a0bc75dd29b
thayer <thayeralshaabi@gmail.com>
2025-06-18 17:41:28 -0400

    fix database connection

--- data/databases/supabase_database.py
+++ data/databases/supabase_database.py
@@ -1,15 +1,12 @@
 import os
-import re
 import sys
 import logging
 from pathlib import Path
-from typing import Optional, Any, List, Tuple, Hashable, Sequence
+from typing import Optional, Any, List, Literal
 
 import pandas as pd
 from dotenv import load_dotenv
 import connectorx as cx
-from supabase import create_client, Client
-from supabase.client import ClientOptions
 
 
 logging.basicConfig(
@@ -23,44 +20,27 @@ logger = logging.getLogger(__name__)
 class SupabaseDatabase:
     def __init__(
         self,
+        dbname: Literal['staging', 'production'] = 'staging',
         dotenv_path: Optional[Path] = Path(__file__).parent.parent.parent / ".env",
     ):
+        self.dbname = dbname
         self.dotenv_path = dotenv_path
-        self.database_url = self._setup_url()
-        self.client = self._setup_client()
+        self.database_url = self._load_uri()
 
-    def _load_creds(self, dotenv_path: Path):
-        assert Path(dotenv_path).exists(), f"{dotenv_path} was not found"
+    def _load_uri(self):
+        assert Path(self.dotenv_path).exists(), f"{self.dotenv_path} was not found"
+        load_dotenv(self.dotenv_path, verbose=True)
 
-        load_dotenv(dotenv_path=dotenv_path, verbose=True)
+        if self.dbname == 'staging':
+            uri = os.environ.get("SUPABASE_STAGING_URI")
+        elif self.dbname == 'production':
+            uri = os.environ.get("SUPABASE_PROD_URI")
+        else:
+            raise ValueError(f"Unknown database name: {self.dbname}")
 
-        url = os.environ.get("SUPABASE_URL")
-        assert url is not None, "SUPABASE_URL environment variable not set in .env file"
+        assert uri is not None, "SUPABASE_URI_* environment variable not set in .env file"
 
-        key = os.environ.get("SUPABASE_KEY")
-        assert key is not None, "SUPABASE_KEY environment variable not set in .env file"
-
-        return url, key #re.escape(key)
-
-    def _setup_url(self, port: int = 5432):
-        url, key = self._load_creds(self.dotenv_path)
-        return f"postgresql://postgres.{url}:{key}@aws-0-us-east-1.pooler.supabase.com:{port}/postgres"
-
-    def _setup_client(self) -> Client:
-        url, key = self._load_creds(self.dotenv_path)
-
-        return create_client(
-            supabase_url=f"https://{url}.supabase.co",
-            supabase_key=key,
-            options=ClientOptions(
-                postgrest_client_timeout=60,
-                storage_client_timeout=60,
-                schema="public",
-            )
-        )
-
-    def test_supabase_client(self) -> Any:
-        return self.client.table("prepared").select("*").execute()
+        return uri
 
     def execute_query(self, query: str) -> pd.DataFrame:
         try:
@@ -71,5 +51,19 @@ class SupabaseDatabase:
             logger.error(f"Failed to execute query: {e}")
             raise
 
-    def test_connection(self) -> Any:
-        return self.execute_query("SELECT * FROM prepared;")
+    def list_tables(self) -> Any:
+        return self.execute_query("SELECT tablename FROM pg_tables WHERE schemaname = 'public';")
+
+    def get_table(self, tablename: str) -> Any:
+        return self.execute_query(f"SELECT * FROM {tablename};")
+
+    def get_columns(self, tablename: str) -> List[str]:
+        return self.execute_query(
+            f"SELECT column_name FROM information_schema.columns WHERE table_name = '{tablename}';"
+        ).column_name.to_list()
+
+    def count_rows(self, tablename: str) -> int:
+        return self.execute_query(f"SELECT COUNT(*) FROM {tablename};").iloc[0, 0]
+
+    def count_columns(self, tablename: str) -> int:
+        return len(self.get_columns(tablename))
