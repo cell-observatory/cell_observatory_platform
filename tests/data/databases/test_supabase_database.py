fba82466c92e15bea682c07429e77fe547e5cf59
thayer <thayeralshaabi@gmail.com>
2025-06-23 10:37:45 -0400

    testing new hypercube views

--- tests/data/databases/test_supabase_database.py
+++ tests/data/databases/test_supabase_database.py
@@ -64,30 +64,60 @@ def test_prepared_cubes_table(database):
 def test_prepared_tiles_view_table(database):
     test_table(database, tablename='prepared_tiles_view')
 
-def test_get_3d_multichannel_hypercubes(database):
-    table = database.get_3d_multichannel_hypercubes(max_tiles=1)
-    print(database.get_3d_multichannel_hypercubes_query)
+def test_get_32_128_128_128_2_hypercubes_with_10_rows(database):
+    table = database.get_32_128_128_128_2_hypercubes(max_rows=10)
+    print(database.last_query)
     print(table)
+    assert table.shape[0] == 10, "Only ten rows should be returned"
+
     pd.testing.assert_series_equal(
-        table['channel_size'],
-        table['channels'].apply(len),
+        table['time_size'],
+        table['timepoints_ch_0'].apply(len),
         check_dtype=False,
         check_names=False,
     )
 
-def test_get_4d_multichannel_hypercubes(database):
-    table = database.get_4d_multichannel_hypercubes(max_tiles=1)
-    print(database.get_4d_multichannel_hypercubes_query)
+
+def test_get_32_128_128_128_2_hypercubes_with_one_roi(database):
+    table = database.get_32_128_128_128_2_hypercubes(max_rois=1)
+    print(database.last_query)
     print(table)
+    assert len(table['prepared_id'].unique()) == 1, "Only one ROI should be returned"
+    assert len(table['tile_name'].unique()) > 1, "More than one tile should be returned"
+
     pd.testing.assert_series_equal(
-        table['channel_size'],
-        table['channels'].apply(len),
+        table['time_size'],
+        table['timepoints_ch_0'].apply(len),
         check_dtype=False,
         check_names=False,
     )
+
+def test_get_32_128_128_128_2_hypercubes_with_one_tile(database):
+    table = database.get_32_128_128_128_2_hypercubes(max_tiles=1)
+    print(database.last_query)
+    print(table)
+
+    assert len(table['prepared_id'].unique()) == 1, "Only one ROI should be returned"
+    assert len(table['tile_name'].unique()) == 1, "Only one tile should be returned"
+
     pd.testing.assert_series_equal(
         table['time_size'],
-        table['timepoints'].apply(len),
+        table['timepoints_ch_0'].apply(len),
+        check_dtype=False,
+        check_names=False,
+    )
+
+def test_get_32_128_128_128_2_hypercubes_with_ten_tile(database):
+    table = database.get_32_128_128_128_2_hypercubes(max_tiles=10)
+    print(database.last_query)
+    print(table)
+
+    assert len(table['prepared_id'].unique()) == 1, "Only one ROI should be returned"
+    assert len(table['tile_name'].unique()) == 10, "Only ten tiles should be returned"
+
+    pd.testing.assert_series_equal(
+        table['time_size'],
+        table['timepoints_ch_0'].apply(len),
         check_dtype=False,
         check_names=False,
     )
