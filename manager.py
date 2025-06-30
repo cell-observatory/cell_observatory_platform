eb0e9e616aca7eaf757f6f13b778bbc5a1a25b55
thayer <thayeralshaabi@gmail.com>
2025-06-30 10:29:38 -0400

    add jobname to each ray worker to not have duplicate job names

--- manager.py
+++ manager.py
@@ -1,6 +1,5 @@
 import os
 import shlex
-import sys
 import subprocess
 from pathlib import Path
 from subprocess import call, run
@@ -27,11 +26,7 @@ os.environ["NCCL_CROSS_NIC"] = "1"
 os.environ["NCCL_P2P_LEVEL"] = "NVL"
 os.environ["TORCH_NCCL_HEARTBEAT_TIMEOUT_SEC"] = "3600"
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
-# TODO: do we want to do this or:
-# load_dotenv(Path(__file__).parent / ".env", verbose=True)?
-# below will load the .env file from pwd vs above will load
-# from repo root
-load_dotenv(verbose=True)
+load_dotenv(Path(__file__).parent / ".env", verbose=True)
 
 def q(x: str) -> str:
     """Shortcut for shlex.quote."""
@@ -105,6 +100,9 @@ def main(cfg: DictConfig):
 
     task = f"{cfg.clusters.python_env} {cfg.paths.runner_script} --config-name {config_name}"
 
+    if cfg.clusters.job_name is None:
+        cfg.clusters.job_name = config_name
+
     ray_wrap = (
         f" bash {q(cfg.paths.ray_script)} "
         f"-b {q(str(bind))} "
@@ -117,6 +115,7 @@ def main(cfg: DictConfig):
         f"-p {q(cfg.clusters.partition)} "
         f"-s {q(str(workspace))} "
         f"-t {q(task)} "
+        f"-j {q(cfg.clusters.job_name)} "
         f"-x {q(cfg.clusters.exclusive)} "
         f"-y {q(cfg.clusters.head_node_gpus)} "
         f"-z {q(cfg.clusters.head_node_cpus)} "
@@ -160,13 +159,8 @@ def main(cfg: DictConfig):
         if cfg.clusters.timelimit is not None:
             sjob_worker_nodes.append(f"--time={cfg.clusters.timelimit}")
 
-        if cfg.clusters.job_name is not None:
-            sjob_worker_nodes.append(f"--job-name={cfg.clusters.job_name}")
-            sjob_worker_nodes.append(f"--output={outdir / cfg.clusters.job_name}.log")
-        else:
-            sjob_worker_nodes.append(f"--job-name=training_job")
-            sjob_worker_nodes.append(f"--output={outdir}/training_job.log")
-
+        sjob_worker_nodes.append(f"--job-name={cfg.clusters.job_name}")
+        sjob_worker_nodes.append(f"--output={outdir / cfg.clusters.job_name}.log")
         sjob_worker_nodes.append(f"--export=ALL")
         sjob_worker_nodes.append(f"--wrap={q(ray_wrap)}")
 
@@ -185,21 +179,16 @@ def main(cfg: DictConfig):
             sjob_worker_nodes.append(f"-x")
         else:
             sjob_worker_nodes.append(f"-n {cfg.clusters.head_node_cpus}")
-            sjob_worker_nodes.append(f'-gpu "num={cfg.clusters.gpus_per_worker}:mode=shared"')
+            sjob_worker_nodes.append(f'-gpu "num={cfg.clusters.head_node_gpus}:mode=shared"')
 
         if cfg.clusters.dependency is not None:
-            sjob_worker_nodes.append(f' -w "done({cfg.clusters.job_name})"')
+            sjob_worker_nodes.append(f'-w "done({cfg.clusters.job_name})"')
 
         if cfg.clusters.timelimit is not None:
-            sjob_worker_nodes.append(f" --We {cfg.clusters.timelimit} ")
+            sjob_worker_nodes.append(f"--We {cfg.clusters.timelimit} ")
 
-        if cfg.clusters.job_name is not None:
-            sjob_worker_nodes.append(f"-J {cfg.clusters.job_name}")
-            sjob_worker_nodes.append(f"-o {outdir / cfg.clusters.job_name}.log")
-        else:
-            sjob_worker_nodes.append(f"-J training_job")
-            sjob_worker_nodes.append(f"-o {outdir}/training_job.log")
-
+        sjob_worker_nodes.append(f"-J {cfg.clusters.job_name}")
+        sjob_worker_nodes.append(f"-o {outdir / cfg.clusters.job_name}.log")
         sjob_worker_nodes.append(f"{q(ray_wrap)}")
 
         print("Checking available Janelia cluster resources...")
@@ -215,8 +204,9 @@ def main(cfg: DictConfig):
             print(f"Error running resource check: {e}")
 
         print("Submitting lsf job with configuration:")
-        print(sjob_worker_nodes)
-        call(sjob_worker_nodes, shell=True)
+        cmd = " ".join(sjob_worker_nodes)
+        print(cmd)
+        subprocess.run(cmd, shell=True, check=True)
 
     else:
         raise ValueError(
