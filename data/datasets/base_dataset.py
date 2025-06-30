420d9444bc7611d185b4882488f0e11d2745e9bd
thayer <thayeralshaabi@gmail.com>
2025-06-30 17:43:56 -0400

    add an option to override server_folder_path

--- data/datasets/base_dataset.py
+++ data/datasets/base_dataset.py
@@ -131,10 +131,20 @@ class BaseDataset(Dataset, metaclass=abc.ABCMeta):
         hypercubes_dataframe_path: Path,
         input_layout: MULTICHANNEL_3D_HYPERCUBE | MULTICHANNEL_4D_HYPERCUBE,
         transforms: Optional[Sequence] = None,
+        server_folder_path: Optional[Path] = None,
     ):
+        """
+        Args:
+            hypercubes_dataframe_path: path to pre-processed hypercubes dataframe from the supabase database
+            input_layout: MULTICHANNEL_3D_HYPERCUBE | MULTICHANNEL_4D_HYPERCUBE
+            transforms: list of optional transforms to apply to each sample (default: None)
+            server_folder_path: path to override default server folder found in the supabase database
+                update this path based on where the data is stored on your local machine
+        """
         super().__init__()
 
         self.input_layout = input_layout
+        self.server_folder_path = server_folder_path
         self.hypercubes_dataframe, self.hypercubes_dataframe_config = self._process_tables(hypercubes_dataframe_path)
 
         self._index = None
@@ -147,6 +157,7 @@ class BaseDataset(Dataset, metaclass=abc.ABCMeta):
         with open(hypercubes_dataframe_path.with_suffix('.json'), 'r') as f:
             configs = ujson.load(f)
 
+        hypercubes['server_folder'] = self.server_folder_path
         return hypercubes, configs
 
     @abc.abstractmethod
