e593d90676e059f6e7f858cd542190a245cb9914
thayer <thayeralshaabi@gmail.com>
2025-07-03 09:15:47 -0400

    combine tests

--- tests/data/datasets/test_pretrain_dataset.py
+++ tests/data/datasets/test_pretrain_dataset.py
@@ -1,47 +1,113 @@
-# import pytest
-# from hydra.utils import instantiate
-# from omegaconf import open_dict
-#
-# import torch
-# from torch.utils.data import DataLoader
-#
-# from tests.conftest import config
-#
-#
-# def test_dataloader(config):
-#     if not torch.cuda.is_available():
-#         pytest.skip("No GPUs available for testing")
-#
-#     with open_dict(config):
-#         config.experiment_name = "test_dataloader"
-#
-#     database = instantiate(config.datasets.databases)
-#     assert database is not None
-#
-#     dataset = instantiate(config.datasets.dataset)
-#
-#     dataloader = DataLoader(
-#         dataset,
-#         collate_fn=instantiate(config.datasets.collate_fn),
-#         batch_size=config.clusters.batch_size_per_gpu,
-#         shuffle=False,
-#         pin_memory=True,
-#         num_workers=config.clusters.cpus_per_worker,
-#         prefetch_factor=2,
-#         persistent_workers=False,
-#         worker_init_fn=dataset.worker_init_fn,
-#         drop_last=True,
-#     )
-#
-#     for idx, data_sample in enumerate(dataloader):
-#         assert isinstance(data_sample, dict), \
-#             f"Data sample {idx} is not a dict, got {type(data_sample)}"
-#
-#         assert "data_tensor" in data_sample and isinstance(data_sample["data_tensor"], torch.Tensor), \
-#             f"Data sample {idx} does not contain 'data_tensor' key or it is not a tensor, got {type(data_sample['data_tensor'])}"
-#
-#         assert "metainfo" in data_sample and isinstance(data_sample["metainfo"], dict), \
-#             f"Data sample {idx} does not contain 'metainfo' key or it is not a dict, got {type(data_sample['metainfo'])}"
-#
-#         if idx >= 5:
-#             break
+
+import pytest
+from pathlib import Path
+from hydra.utils import instantiate
+from omegaconf import open_dict
+from hydra.utils import get_class
+
+import torch
+from torch.utils.data import DataLoader
+
+from tests.conftest import distributed_test, config
+
+
+def test_access_to_storage_server(config):
+    if not Path(config.paths.server_folder_path).exists():
+        raise FileNotFoundError(f"{config.paths.server_folder_path} does not exist")
+
+
+def test_dataloader(config):
+    if not torch.cuda.is_available():
+        pytest.skip("No GPUs available for testing")
+
+    with open_dict(config):
+        config.experiment_name = "test_dataloader"
+
+    database = instantiate(config.datasets.databases)
+    assert database is not None
+
+    dataset = instantiate(config.datasets.dataset)
+
+    dataloader = DataLoader(
+        dataset,
+        collate_fn=instantiate(config.datasets.collate_fn),
+        batch_size=1,
+        shuffle=False,
+        pin_memory=True,
+        num_workers=1,
+        prefetch_factor=2,
+        persistent_workers=False,
+        worker_init_fn=dataset.worker_init_fn,
+        drop_last=True,
+    )
+
+    for idx, data_sample in enumerate(dataloader):
+        assert isinstance(data_sample, dict), \
+            f"Data sample {idx} is not a dict, got {type(data_sample)}"
+
+        assert "data_tensor" in data_sample and isinstance(data_sample["data_tensor"], torch.Tensor), \
+            f"Data sample {idx} does not contain 'data_tensor' key or it is not a tensor, got {type(data_sample['data_tensor'])}"
+
+        assert "metainfo" in data_sample and isinstance(data_sample["metainfo"], dict), \
+            f"Data sample {idx} does not contain 'metainfo' key or it is not a dict, got {type(data_sample['metainfo'])}"
+ 
+        expected_shape = (
+            data_sample['metainfo']["time_size"],
+            data_sample['metainfo']["cube_size"],
+            data_sample['metainfo']["cube_size"],
+            data_sample['metainfo']["cube_size"],
+            data_sample['metainfo']["channel_size"]
+        )
+        assert data_sample['data_tensor'][0].shape == expected_shape, \
+            f"Data tensor shape {data_sample['data_tensor'][0].shape} does not match expected shape {expected_shape}"
+        
+        
+        if idx >= 5:
+            break
+
+
+def _test_dataloader_dist(config):
+    trainer_cls = get_class(config.trainer)
+    trainer = trainer_cls(config)
+
+    for idx, data_sample in enumerate(trainer.train_dataloader):
+
+        assert isinstance(data_sample, dict), f"Data sample {idx} is not a dict, got {type(data_sample)}"
+
+        assert "data_tensor" in data_sample and isinstance(data_sample["data_tensor"], torch.Tensor), \
+            f"Data sample {idx} does not contain 'data_tensor' key or it is not a tensor, got {type(data_sample['data_tensor'])}"
+
+        assert "metainfo" in data_sample and isinstance(data_sample["metainfo"], dict), \
+            f"Data sample {idx} does not contain 'metainfo' key or it is not a dict, got {type(data_sample['metainfo'])}"
+
+        expected_shape = (
+            data_sample['metainfo']["time_size"],
+            data_sample['metainfo']["cube_size"],
+            data_sample['metainfo']["cube_size"],
+            data_sample['metainfo']["cube_size"],
+            data_sample['metainfo']["channel_size"]
+        )
+        assert data_sample['data_tensor'][0].shape == expected_shape, \
+            f"Data tensor shape {data_sample['data_tensor'][0].shape} does not match expected shape {expected_shape}"
+
+        if idx >= 5:
+            break
+
+    return {"success": True}
+
+
+def test_data_pipeline(config):
+    if not torch.cuda.is_available():
+        pytest.skip("No GPUs available for testing")
+
+    with open_dict(config):
+        config.experiment_name = "test_data_pipeline"
+        config.paths.resume_checkpointdir = None
+
+        config.clusters.worker_nodes = 1
+        config.clusters.gpus_per_worker = torch.cuda.device_count()
+        config.clusters.cpus_per_gpu = 4
+        config.clusters.mem_per_cpu = 31000
+
+    metrics = distributed_test(cfg=config, test="tests.data.datasets.test_pretrain_dataset._test_dataloader_dist")
+    assert metrics.get("success", True), "Distributed dataloader test failed"
