bfb848f6d4bc27c486f03ada5a3b4c120972ec13
thayer <thayeralshaabi@gmail.com>
2025-07-08 14:18:44 -0400

    disable pytest for testing_loop

--- tests/training/test_loops.py
+++ tests/training/test_loops.py
@@ -42,6 +42,7 @@ def test_train_loop(config):
     assert metrics.get("success", False), "Distributed loops test failed"
 
 @pytest.mark.order(2)
+@pytest.mark.skip(reason="This test is temporarily disabled.")
 def test_testing_loop(config):
     if not torch.cuda.is_available():
         pytest.skip("No GPUs available for testing")
@@ -49,15 +50,19 @@ def test_testing_loop(config):
     with open_dict(config):
         config.experiment_name = "test_testing_loop"
         config.paths.resume_checkpointdir = None
-        # we need a checkpoint directory for testing
-        # so we use the last checkpoint created from testing the training loop
-        config.paths.pretrained_checkpointdir = os.path.join(Path(config.paths.outdir).parent, 
-                                                             "test_train_loop", 
-                                                             "checkpoints")
+
+        # TODO: may need a checkpoint directory for testing or a dummy checkpoint
+        config.paths.pretrained_checkpointdir = os.path.join(
+            Path(config.paths.outdir).parent,
+            "test_train_loop",
+            "checkpoints"
+        )
         config.checkpoint.checkpoint_manager.checkpoint_tag = "latest_model"
 
         config.trainer = "training.loops.TestTrainer"
         config.evaluation.val_metric = "test_step_loss"
+
+        # config.datasets.databases.max_hypercubes = 10000
 
     metrics = distributed_test(cfg=config, test="tests.training.test_loops._test_testing_loop_dist")
     assert metrics.get("success", False), "Distributed loops test failed"
