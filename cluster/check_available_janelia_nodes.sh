ecc06a19f848b25486fe2b5072fd5617af5f382b
thayer <thayeralshaabi@gmail.com>
2025-07-22 17:14:55 -0400

    fix checks

--- cluster/check_available_janelia_nodes.sh
+++ cluster/check_available_janelia_nodes.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # parse args from `args_parser.sh` getopts
-source ./args_parser.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$DIR/args_parser.sh"
 
 alias myjobs="watch bjobs"
 
@@ -15,26 +16,26 @@ alias h200jobs="watch bjobs -q gpu_h200 -u all"
 
 if [ $partition = 'gpu_h200' ];then
     AVAL=$(bhosts -o "host_name run:-6"  h200s | grep -w "0" | awk '{print $1}' | wc -l)
-    while [ $AVAL -lt $NODES ]
+    while [ $AVAL -lt $nodes ]
     do
         sleep 1
-        echo "Waiting for [$AVAL/$NODES] nodes to be available"
-        AVAL=$(bhosts -o "host_name run:-6"  h100s | grep -w "0" | awk '{print $1}' | wc -l)
+        echo "Waiting for [$AVAL/$nodes] nodes to be available on $partition queue"
+        AVAL=$(bhosts -o "host_name run:-6"  h200s | grep -w "0" | awk '{print $1}' | wc -l)
     done
 elif [ $partition = 'gpu_h100' ];then
     AVAL=$(bhosts -o "host_name run:-6"  h100s | grep -w "0" | awk '{print $1}' | wc -l)
-    while [ $AVAL -lt $NODES ]
+    while [ $AVAL -lt $nodes ]
     do
         sleep 1
-        echo "Waiting for [$AVAL/$NODES] nodes to be available"
+        echo "Waiting for [$AVAL/$nodes] nodes to be available on $partition queue"
         AVAL=$(bhosts -o "host_name run:-6"  h100s | grep -w "0" | awk '{print $1}' | wc -l)
     done
 elif [ $partition = 'gpu_a100' ];then
     AVAL=$(bhosts -o "host_name run:-6"  a100s | grep -w "0" | awk '{print $1}' | wc -l)
-    while [ $AVAL -lt $NODES ]
+    while [ $AVAL -lt $nodes ]
     do
         sleep 1
-        echo "Waiting for [$AVAL/$NODES] nodes to be available"
+        echo "Waiting for [$AVAL/$nodes] nodes to be available on $partition queue"
         AVAL=$(bhosts -o "host_name run:-6"  a100s | grep -w "0" | awk '{print $1}' | wc -l)
     done
 fi
