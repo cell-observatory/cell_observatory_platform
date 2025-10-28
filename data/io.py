fix multi-block masking bug

data/io.py
@@ -375,7 +375,8 @@ def _string_seq_to_float_list(value: Any) -> list[float]:
 
 def apply_occupancy_threshold(
     hypercubes_dataframe: pd.DataFrame,
-    occupancy_threshold: Optional[float] = 0.
+    occupancy_threshold: Optional[float] = 0.,
+    occupancy_threshold_filter_type: Literal['min_all', 'min_ch0', 'min_ch1'] = 'min_ch0'
 ):
     t = 0. if occupancy_threshold is None else occupancy_threshold
 
@@ -391,19 +392,32 @@ def apply_occupancy_threshold(
     hypercubes_dataframe['min_occupancy_ratios_ch_1'] = hypercubes_dataframe['occupancy_ratios_ch_1'].apply(np.min)
     hypercubes_dataframe['med_occupancy_ratios_ch_1'] = hypercubes_dataframe['occupancy_ratios_ch_1'].apply(np.median)
 
-    return hypercubes_dataframe[
-        (hypercubes_dataframe['min_occupancy_ratios_ch_0'] >= t) &
-        (hypercubes_dataframe['min_occupancy_ratios_ch_1'] >= t)
-    ]
+    if occupancy_threshold_filter_type == 'min_all':
+        return hypercubes_dataframe[
+            (hypercubes_dataframe['min_occupancy_ratios_ch_0'] >= t) &
+            (hypercubes_dataframe['min_occupancy_ratios_ch_1'] >= t)
+        ]
+    elif occupancy_threshold_filter_type == 'min_ch0':
+        return hypercubes_dataframe[
+            (hypercubes_dataframe['min_occupancy_ratios_ch_0'] >= t)
+        ]
+    elif occupancy_threshold_filter_type == 'min_ch1':
+        return hypercubes_dataframe[
+            (hypercubes_dataframe['min_occupancy_ratios_ch_1'] >= t)
+        ]
+    else:
+        raise ValueError(f"Unknown occupancy_threshold_filter_type: {occupancy_threshold_filter_type}")
 
 
 def apply_hypercubes_dataframe_filters(
     hypercubes_dataframe: pd.DataFrame,
-    occupancy_threshold: Optional[float] = 0.
+    occupancy_threshold: Optional[float] = 0.,
+    occupancy_threshold_filter_type: str = 'min_all'
 ):
     hypercubes_dataframe = apply_occupancy_threshold(
         hypercubes_dataframe=hypercubes_dataframe,
         occupancy_threshold=occupancy_threshold,
+        occupancy_threshold_filter_type=occupancy_threshold_filter_type
     )
 
     logger.info(hypercubes_dataframe[['min_occupancy_ratios_ch_0', 'min_occupancy_ratios_ch_1']].describe(
@@ -423,7 +437,8 @@ def load_hypercubes_dataframe(
     tile_list: Optional[Iterable[str]] = None,
     timepoint_list: Optional[Iterable[int]] = None,
     server_folder_path: Optional[Path | str] = None,
-    occupancy_threshold: Optional[float] = None
+    occupancy_threshold: Optional[float] = None,
+    occupancy_threshold_filter_type: str = 'min_all'
 ) -> Tuple[pd.DataFrame, Dict]:
 
     if not Path(hypercubes_dataframe_path).exists():
@@ -453,6 +468,7 @@ def load_hypercubes_dataframe(
     hypercubes = apply_hypercubes_dataframe_filters(
         hypercubes_dataframe=hypercubes,
         occupancy_threshold=occupancy_threshold,
+        occupancy_threshold_filter_type=occupancy_threshold_filter_type
     )
 
     logger.info(f"Loaded hypercubes dataframe with {hypercubes.shape}")

data/masking/mask_generator.py
@@ -467,7 +467,11 @@ class MaskGenerator(object):
         else:
             raise ValueError(f"Unknown mask mode: {self.mask_mode}")
 
-        return masks, context_masks, target_masks, original_patch_indices, self.channels_to_mask
+        # perm: [B, patches_used]
+        perm = torch.cat([context_masks, target_masks], dim=1)
+        patches_used, _ = torch.sort(perm, dim=1) 
+
+        return masks, context_masks, target_masks, original_patch_indices, self.channels_to_mask, patches_used
 
 
 def apply_masks(x, masks, concat=True):

models/jepa.py
@@ -316,21 +316,27 @@ class JEPA(nn.Module):
 
     def forward(self, data_sample: dict):
         inputs, meta = data_sample['data_tensor'], data_sample['metainfo']
-        masks, context_masks = meta['masks'][0], meta['context_masks'][0]
+        masks, context_masks, patches_used = meta['masks'][0], meta['context_masks'][0], meta['patches_used'][0]
         target_masks, original_patch_indices = meta['target_masks'][0], meta['original_patch_indices'][0]
 
         embedding, patches = self.input_encoder(inputs, masks=context_masks)
         predictions = self.target_predictor(
             embedding,
             original_patch_indices=original_patch_indices,
-            target_masks=target_masks
+            target_masks=target_masks,
+            patches_used=patches_used
         )
 
         with torch.no_grad():
             targets, _ = self.target_encoder(inputs)
 
+        # compute loss over masked patches (re-index if blocked masking removed some patches)
+        if patches_used is not None:
+            target_idx_in_patches_used = torch.searchsorted(patches_used, target_masks)
+        else:
+            target_idx_in_patches_used = target_masks
         targets = apply_masks(targets, masks=target_masks)
-        predictions = apply_masks(predictions, masks=target_masks)
+        predictions = apply_masks(predictions, masks=target_idx_in_patches_used)
         loss = self.loss_fn(predictions, targets, masks)
 
         loss_dict = {

models/maskedpredictor.py
@@ -236,8 +236,7 @@ class MaskedPredictor(nn.Module):
             )
             return num_patches
 
-
-    def forward(self, inputs, original_patch_indices=None, target_masks=None):
+    def forward(self, inputs, original_patch_indices=None, target_masks=None, patches_used=None):
         batch_size = inputs.shape[0]
 
         tokens = self.patch_projection(inputs)
@@ -253,11 +252,11 @@ class MaskedPredictor(nn.Module):
             patches = tokens
 
         if self.abs_sincos_enc:
-            x = patches + self.pos_embedding(patches)
+            x = patches + self.pos_embedding(patches, patches_used=patches_used)
         else:
             x = patches
 
-        x = self.encoder(x)
+        x = self.encoder(x, masks=patches_used)
         x = self.norm(x)
         x = self.output_projection(x)
         return x

models/positional_encoding.py
@@ -210,6 +210,7 @@ class PosEmbedding(nn.Module):
         self.embed_dim = embed_dim
         self.channels = channels
         self.cls_token = cls_token
+        assert not self.cls_token, "CLS token not yet supported for PosEmbedding"
         self.interpolate = interpolate
 
         self.num_patches, self.token_shape = patch_embeddings.calc_num_patches(
@@ -390,8 +391,40 @@ class PosEmbedding(nn.Module):
 
         return pos
 
-    def forward(self, x):
+    def gather_pos_table(self, pos_table, patches_used):
+        idx = patches_used.to(pos_table.device)
+        D = pos_table.size(-1)
+        # pos_table_batched: (B, L_full, D) -> (B, L_used, D)
+        out = torch.gather(pos_table, 
+                            dim=1,
+                            index=idx.unsqueeze(-1).expand(-1, -1, D))
+        return out
+
+    @property
+    def table(self) -> torch.Tensor:
+        # (L_full, D) without cls
+        return (self.pos_embed[:, 1:, :]
+                if self.cls_token else
+                self.pos_embed)
+
+    def forward(self, x: Optional[torch.Tensor] = None,
+                patches_used: Optional[torch.Tensor] = None) -> torch.Tensor:
         if self.interpolate:
-            return self.interpolate_positional_encoding(x, self.pos_embed)
-        else:
-            return self.pos_embed
+            # FIXME: interpolate_positional_encoding assumes x is grid format
+            #        but currently we pass in sequence format from modules
+            pos_table_interpolated = self.interpolate_positional_encoding(x, self.pos_embed)
+
+            if patches_used is not None:
+                # NOTE: gather_pos_table assumes batched pos_table
+                pos_table_subset = self.gather_pos_table(pos_table_interpolated, patches_used)
+                return pos_table_subset
+            else:
+                return pos_table_interpolated
+
+        if patches_used is not None:
+            pos_table = self.table
+            B, L_used = patches_used.shape
+            pos_table_batched = pos_table.expand(B, -1, -1)
+            return self.gather_pos_table(pos_table_batched, patches_used)
+
+        return self.pos_embed

models/rope.py
@@ -16,7 +16,6 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-
 def generate_frequency_spectrum(dim: int, 
                                 num_heads: int, 
                                 theta: float = 10.0, 
@@ -110,6 +109,7 @@ def generate_frequency_spectrum(dim: int,
     
     return freqs.to(dtype=dtype, device=device)
 
+
 def generate_grid_indices(
     end_x: int,
     end_y: int,
@@ -150,6 +150,7 @@ def generate_grid_indices(
     x = x.to(dtype=dtype, device=device)
 
     return (t, z, y, x)
+
 
 def compute_mixed_cis(freqs: torch.Tensor,
                       num_heads: int,
@@ -305,6 +306,7 @@ def compute_axial_cis(dim: int,
         freqs_cis_t = torch.polar(torch.ones_like(freqs_t), freqs_t)
         return torch.cat([freqs_cis_x, freqs_cis_y, freqs_cis_z, freqs_cis_t], dim=-1)
 
+
 def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
     # freqs_cis: (N, J) branch
     if freqs_cis.shape == (x.shape[-2], x.shape[-1]):
@@ -323,6 +325,7 @@ def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
     else:
         raise ValueError(f"Unexpected freqs_cis shape: {freqs_cis.shape} for x shape: {x.shape}")
     return freqs_cis.view(*shape)
+
 
 def apply_rotary_emb(xq: torch.Tensor, xk: torch.Tensor, freqs_cis: torch.Tensor):
     # xq: [B,H,N,D]