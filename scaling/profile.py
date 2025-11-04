ce5ef2cc8178cc11f3812d48e060d448e3a2da17
thayer <thayeralshaabi@gmail.com>
2025-11-04 22:46:12 +0000

    add mfu

--- scaling/profile.py
+++ scaling/profile.py
@@ -108,7 +108,7 @@ def encoder_transformer_flops(num_tokens, layers, embed_dim, heads, mlp_dim):
     # flops += patchify_flops(num_tokens, patch_size, embed_dim)
 
     gflops = np.round(flops / 1e9, 3)
-    logger.info(f"{flops:,} FLOPs = {gflops} GFLOPs")
+    logger.info(f"encoder transformer: {flops:,} FLOPs = {gflops} GFLOPs")
     return flops
 
 
@@ -117,7 +117,7 @@ def decoder_transformer_flops(num_tokens, layers, embed_dim, heads, mlp_dim):
     # flops += patchify_flops(num_tokens, patch_size, embed_dim)
 
     gflops = np.round(flops / 1e9, 3)
-    logger.info(f"{flops:,} FLOPs = {gflops} GFLOPs")
+    logger.info(f"decoder transformer: {flops:,} FLOPs = {gflops} GFLOPs")
     return flops
 
 
@@ -129,7 +129,7 @@ def encoder_transformer_params(layers, embed_dim, mlp_dim):
     encoder = layers * (attention + feed_forward + 2 * layer_norm)
     encoder_mparams = np.round(encoder / 1e6, 0).astype(int)
 
-    logger.info(f"{encoder:,} params = {encoder_mparams} M params")
+    logger.info(f"encoder transformer: {encoder:,} params = {encoder_mparams} M params")
     # feed_forward = 8 * embed_dim**2 + 5 * embed_dim
     # attention = (4 * embed_dim ** 2 + 4 * embed_dim) * 1
     # layer_norm = (2 * embed_dim) * 2
@@ -145,7 +145,7 @@ def decoder_transformer_params(layers, embed_dim, mlp_dim):
     decoder = layers * (2 * attention + feed_forward + 3 * layer_norm)
     decoder_mparams = np.round(decoder / 1e6, 0).astype(int)
 
-    logger.info(f"{decoder:,} params = {decoder_mparams} M params")
+    logger.info(f"decoder transformer: {decoder:,} params = {decoder_mparams} M params")
     # feed_forward = 8 * embed_dim**2 + 5 * embed_dim
     # attention = 8 * embed_dim ** 2 + 8 * embed_dim
     # layer_norm = 6 * embed_dim
@@ -153,7 +153,7 @@ def decoder_transformer_params(layers, embed_dim, mlp_dim):
     return decoder
 
 
-def transformer_inference_memory_footprint(params, dtype='float32'):
+def transformer_inference_memory_footprint(params, dtype='float16'):
     if dtype == 'float16':
         s = 2.0
     elif dtype == 'float32':
@@ -166,11 +166,11 @@ def transformer_inference_memory_footprint(params, dtype='float32'):
     mem = int(s * params)
     gbytes = mem / (1024.0 ** 3)
 
-    logger.info(f"{mem:,d} B = {gbytes} GB using ({dtype})")
+    logger.info(f"inference memory: {mem:,d} B = {gbytes} GB using ({dtype})")
     return gbytes
 
 
-def transformer_training_memory_footprint(params, dtype='float32'):
+def transformer_training_memory_footprint(params, dtype='float16'):
     if dtype == 'float16':
         s = 2.0
     elif dtype == 'float32':
@@ -187,11 +187,11 @@ def transformer_training_memory_footprint(params, dtype='float32'):
     mem = int(model + grads + opt)
     gbytes = mem / (1024.0 ** 3)
 
-    logger.info(f"{mem:,d} B = {gbytes} GB using ({dtype})")
+    logger.info(f"training memory: {mem:,d} B = {gbytes} GB using ({dtype})")
     return gbytes
 
 
-def data_memory_footprint(volume_size, batch_size=1, dtype='float32'):
+def data_memory_footprint(volume_size, batch_size=1, dtype='float16'):
     if dtype == 'float8':
         s = 1.0
     elif dtype == 'float16':
@@ -206,14 +206,14 @@ def data_memory_footprint(volume_size, batch_size=1, dtype='float32'):
     mem = int(s * batch_size * np.product(volume_size))
     gbytes = mem / (1024.0 ** 3)
 
-    logger.info(f"{mem:,d} B = {gbytes} GB using ({dtype})")
+    logger.info(f"data memory: {mem:,d} B = {gbytes} GB using ({dtype})")
     return gbytes
 
 
-def compute_time(flops, gpu="H100", unit="seconds"):
+def compute_time(flops, gpu="H100", unit="seconds", mfu=.5):
     """
     Google benchmark
-    https://github.com/GoogleCloudcell_observatory_platform/vertex-ai-samples/blob/main/community-content/vertex_model_garden/benchmarking_reports/jax_vit_benchmarking_report.md
+    https://github.com/GoogleCloudPlatform/vertex-ai-samples/blob/main/community-content/vertex_model_garden/benchmarking_reports/jax_vit_benchmarking_report.md
     # g/14 533, GFLOPs, 1066 training GFLOPs
     # GFLOPS = GFLOPs / sec/img/GPU
     """
@@ -221,25 +221,34 @@ def compute_time(flops, gpu="H100", unit="seconds"):
         # https://cloud.google.com/tpu/docs/v3
         # 18 images/sec
         # t = 0.055268 sec/img/GPU
-        average_utilization = 19 * 10 ** 12
+        # average_utilization = 19 * 10 ** 12
+        peak_theoretical_flops = 123 * 10 ** 12
 
     elif gpu == "A100":
+        # https://www.nvidia.com/en-us/data-center/a100/
         # https://images.nvidia.com/aem-dam/en-zz/Solutions/data-center/nvidia-ampere-architecture-whitepaper.pdf
         # 51 images/sec
         # t = 0.019608 sec/img/GPU
-        average_utilization = 54 * 10 ** 12
+        # average_utilization = 54 * 10 ** 12
+        peak_theoretical_flops = 312 * 10 ** 12
 
     elif gpu == "H100":
-        # https://resources.nvidia.com/en-us-tensor-core
-        # x2 A100
-        average_utilization = 109 * 10 ** 12
+        # https://www.nvidia.com/en-us/data-center/h100/
+        # https://resources.nvidia.com/en-us-hopper-architecture/nvidia-h100-tensor-c
+        # ~x2 A100
+        # average_utilization = 109 * 10 ** 12
+        peak_theoretical_flops = 989.4 * 10 ** 12
 
     else:
         raise Exception("Unknown GPU device")
 
-    time = flops / average_utilization
+    effective_flops = peak_theoretical_flops * mfu
+    time = flops / effective_flops
+    
     if unit == "hours":
         time = time / (60 * 60)
     elif unit == "minutes":
         time = time / 60
+        
+    logger.info(f"compute time: {time} {unit} using ({gpu}, mfu={mfu})")
     return time
