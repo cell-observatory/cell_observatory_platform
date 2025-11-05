8b04808c0200dabd52cf97b28adb37a6dd48a5d9
Hugo Hamilton <hph@berkeley.edu>
2025-11-05 12:16:10 -0800

    update scripts

--- scripts/utils/login.sh
+++ scripts/utils/login.sh
@@ -1,15 +1,4 @@
-#!/bin/bash
-
-# NOTE: when image is ready add the following line:
-#    --image cell-observatory
-# for now run:
-#   uv pip install hydra dotenv omegaconf
-
-ai dev submit \
-    --project cell-observatory \
-    --name debug_job \
-    --gpus 0 \
-    --cpus-limit 8 \
-    --data project-cell-observatory-pvc-120t=/workspace/CellObservatoryData \
-    --data project-cell-observatory-pvc-120t=/workspace/cell_observatory_project \
-    --repo https://github.com/czi-ai/cell-observatory
+# !/bin/bash
+# NOTE: when image is ready add the following line: --image cell-observatory
+# for now run: uv pip install hydra dotenv omegaconf
+ai dev submit --project cell-observatory --ide-type code --name debug --gpus 0 --cpus-limit 8 --data project-cell-observatory-pvc-120t=/workspace/CellObservatoryData --data project-cell-observatory-pvc-120t=/workspace/cell_observatory_project --repo https://github.com/czi-ai/cell-observatory
