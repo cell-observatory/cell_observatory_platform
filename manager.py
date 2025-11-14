368cae5aea4b4a51182b4895c0c935c3f7225742
thayer <thayeralshaabi@gmail.com>
2025-11-14 18:33:56 +0000

    fix path

--- manager.py
+++ manager.py
@@ -445,7 +445,7 @@ def launch_job(cfg: DictConfig, run_config_name: str = None):
             sjob_worker_nodes.append(f"--We {cfg.clusters.timelimit} ")
 
         sjob_worker_nodes.append(f"-J {cfg.clusters.job_name}")
-        sjob_worker_nodes.append(f"-o {outdir / cfg.clusters.job_name}.log")
+        sjob_worker_nodes.append(f"-o {outdir}/{cfg.clusters.job_name}.log")
         sjob_worker_nodes.append(f"-env 'all'")
         sjob_worker_nodes.append(f"{q(ray_wrap)}")
 
