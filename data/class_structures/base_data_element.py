55df4ec6f02240cf04f278d2949684e5c159901e
thayer <thayeralshaabi@gmail.com>
2025-12-09 23:03:05 +0000

    add missing import

--- data/class_structures/base_data_element.py
+++ data/class_structures/base_data_element.py
@@ -18,6 +18,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import copy
 import logging
 import sys
 from typing import Any, Iterator, Optional, Tuple, Type, Union
