ff40981d8bfd94026072cfab2cc05cf401a54e32
thayer <thayeralshaabi@gmail.com>
2025-12-09 21:46:49 +0000

    fix imports

--- data/datasets/base_dataset.py
+++ data/datasets/base_dataset.py
@@ -1,26 +1,22 @@
 import abc
+import logging
 import sys
 import time
-import logging
-from typing import Any, Callable, Optional, Sequence, Mapping, Dict, Literal, Iterable
 from pathlib import Path
-import ujson
-import pandas as pd
+from typing import Any, Callable, Dict, Iterable, Literal, Mapping, Optional, Sequence
 
+import pandas as pd
+import ujson
 from torch.utils.data import Dataset
 from torch.utils.data._utils.collate import default_collate as torch_default_collate
 
-from cell_observatory_platform.data.structures.data_sample import DataSample
-from cell_observatory_platform.data.structures.image_list import cat_image_lists
+from cell_observatory_platform.data.class_structures.data_sample import DataSample
+from cell_observatory_platform.data.class_structures.image_list import cat_image_lists
 from cell_observatory_platform.data.data_shapes import MULTICHANNEL_HYPERCUBE
-from cell_observatory_platform.data.data_types import TENSORSTORE_DTYPES, NUMPY_DTYPES, TORCH_DTYPES
+from cell_observatory_platform.data.data_types import NUMPY_DTYPES, TENSORSTORE_DTYPES, TORCH_DTYPES
 from cell_observatory_platform.data.io import load_hypercubes_dataframe
 
-logging.basicConfig(
-    stream=sys.stdout,
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
 
@@ -28,18 +24,18 @@ logger = logging.getLogger(__name__)
 def default_collate(data_batch: Sequence) -> Any:
     """
     Convert list of data sampled from dataset into
-    a batch of data, of type consistent with 
+    a batch of data, of type consistent with
     the type of each element in ``data_batch``.
 
     Args:
         data_batch (Sequence): Data sampled from dataset.
 
     Returns:
-        Any: Data in the same format as the data element 
-        of ``data_batch``, of which tensors have been 
+        Any: Data in the same format as the data element
+        of ``data_batch``, of which tensors have been
         stacked, and ndarray, int, float have been
         converted to tensors, etc.
-    """  
+    """
     # NOTE: we assume the each data element in data_batch
     #       is of the same type
     data_item = data_batch[0]
@@ -48,34 +44,30 @@ def default_collate(data_batch: Sequence) -> Any:
     # recursive collate
     if isinstance(data_item, (str, bytes)):
         return data_batch
-    elif isinstance(data_item, tuple) and hasattr(data_item, '_fields'):
+    elif isinstance(data_item, tuple) and hasattr(data_item, "_fields"):
         # named tuple
         # we transpose the batch to get a tuple of lists
         # recursively collate each list, then lastly
         # rebuild same named tuple type
-        return data_item_type(*(default_collate(samples)
-                                for samples in zip(*data_batch)))
+        return data_item_type(*(default_collate(samples) for samples in zip(*data_batch)))
     elif isinstance(data_item, Sequence):
-        # check to make sure that the elements 
+        # check to make sure that the elements
         # in batch have consistent size
         it = iter(data_batch)
         data_item_size = len(next(it))
         if not all(len(data_item) == data_item_size for data_item in it):
-            raise RuntimeError(
-                'each data_itement in list of batch should be of equal size')
-        
+            raise RuntimeError("each data_itement in list of batch should be of equal size")
+
         # from [(a, b), (c, d)] to [(a, c), (b, d)]
         transposed = list(zip(*data_batch))
 
         # from [(a, c), (b, d)] to [collated[a, c], collated[b, d]]
         if isinstance(data_item, tuple):
             # compatible with Pytorch
-            return [default_collate(samples)
-                    for samples in transposed]  
+            return [default_collate(samples) for samples in transposed]
         else:
             try:
-                return data_item_type(
-                    [default_collate(samples) for samples in transposed])
+                return data_item_type([default_collate(samples) for samples in transposed])
             except TypeError:
                 # sequence type may not support `__init__(iterable)`
                 # (e.g., `range`). Fall back to list.
@@ -84,10 +76,7 @@ def default_collate(data_batch: Sequence) -> Any:
         # [{"img": img1, "label": label1},
         #  {"img": img2, "label": label2}]
         # to {"img": collate[img1, img2], "label": collate[label1, label2]}
-        return data_item_type({
-            key: default_collate([d[key] for d in data_batch])
-            for key in data_item
-        })
+        return data_item_type({key: default_collate([d[key] for d in data_batch]) for key in data_item})
     else:
         return torch_default_collate(data_batch)
 
@@ -210,8 +199,8 @@ class BaseDataset(Dataset, metaclass=abc.ABCMeta):
 
     def __getitem__(self, idx: int):
         if self.time:
-            start_time = time.time() 
-        
+            start_time = time.time()
+
         _data = self._load_sample(self._index[idx])
         data = self._collate(_data)
         data_transformed = self.transforms(data)
