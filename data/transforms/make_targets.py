2d71e46b2038e6d26e70491b6258522178a89e69
panzax <martin.alvarez.kuglen@gmail.com>
2026-01-26 16:50:17 -0800

    fix: removed requirement that targets not exist in metainfo, as it is generated earlier in the pipeline.

--- data/transforms/make_targets.py
+++ data/transforms/make_targets.py
@@ -9,7 +9,7 @@ class DeepCopyInputsAsTargets:
             raise KeyError("DeepCopyInputsAsTargets expects 'data_tensor' in dict.")
         if "metainfo" not in data:
             data["metainfo"] = {}
-        if "targets" in data["metainfo"] and data["metainfo"]["targets"] is not None:
-            raise ValueError("targets already exists in metainfo")
+        # if "targets" in data["metainfo"] and data["metainfo"]["targets"] is not None:
+        #     raise ValueError("targets already exists in metainfo")
         data["metainfo"]["targets"] = [data["data_tensor"].clone()]
         return data
