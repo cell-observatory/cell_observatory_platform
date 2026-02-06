89513e6274e04d4a12a9c2b850c550a64f5ecbf8
thayer <thayeralshaabi@gmail.com>
2026-02-06 17:08:15 +0000

    add Ep flag

--- manager.py
+++ manager.py
@@ -443,7 +443,8 @@ def launch_job(cfg: DictConfig, run_config_name: str = None):
 
         if cfg.clusters.timelimit is not None:
             sjob_worker_nodes.append(f"--We {cfg.clusters.timelimit} ")
-
+        
+        sjob_worker_nodes.append(f"-Ep 'bash {cfg.paths.repo_path}/cluster/clean_shm.sh'")
         sjob_worker_nodes.append(f"-J {cfg.clusters.job_name}")
         sjob_worker_nodes.append(f"-o {outdir}/{cfg.clusters.job_name}.log")
         sjob_worker_nodes.append(f"-env 'all'")
