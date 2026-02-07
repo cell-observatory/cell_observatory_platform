ba64f566f118c74d3277d47dc0c97a9102e688d3
panzax <martin.alvarez.kuglen@gmail.com>
2026-02-06 16:20:15 -0800

    feat: Add ForgroundMasks transform for generating foreground masks from label maps. Optionally remove boundary labels

--- data/transforms/make_targets.py
+++ data/transforms/make_targets.py
@@ -88,3 +88,49 @@ class InstanceToBoundaryMask:
             boundary[:, z0:z1, y0:y1, x0:x1] |= (a != b)
 
         return boundary
+
+
+class ForgroundMasks:
+    def __init__(self, remove_boundary: bool = True):
+        self.remove_boundary = remove_boundary
+
+    def __call__(self, data: dict | torch.Tensor) -> dict | torch.Tensor:
+        if isinstance(data, dict):
+            if "masks_labelmap" not in data:
+                raise KeyError("ForgroundMasks expects 'masks_labelmap' in dict.")
+            if self.remove_boundary:
+                if "boundary_masks" not in data:
+                    raise KeyError("ForgroundMasks expects 'boundary_masks' in dict.")
+                forground_masks = self._forground_masks(data["masks_labelmap"], data["boundary_masks"])
+            else:
+                forground_masks = self._forground_masks(data["masks_labelmap"])
+            data["forground_masks"] = forground_masks
+            return data
+        elif isinstance(data, torch.Tensor):
+            if self.remove_boundary:
+                raise ValueError("ForgroundMasks does not support tensor input if remove_boundary=True. Use dict input containing key 'boundary_masks' instead.")
+            else:
+                self._forground_masks(data)
+        else:
+            raise ValueError(f"ForgroundWithoutBoundaryMask expects dict or torch.Tensor, got {type(data)}")
+        
+    def _forground_masks(self, labels: torch.Tensor, boundary_masks: torch.Tensor | None = None) -> torch.Tensor:
+        """
+        labels: (B,D,H,W) int (instance/semantic labels; background can be 0).
+        boundary_masks: (B,D,H,W) bool (boundary masks).
+        Returns: bool mask of same shape, True where boundaries occur.
+        """
+        if labels.dim() != 4:
+            raise ValueError(f"labels must be a 4D tensor assumed to be (B,D,H,W), got {labels.shape}")
+        
+        B, D, H, W = labels.shape
+        x = labels
+
+        forground_masks = torch.zeros((B, D, H, W), dtype=torch.bool, device=x.device)
+        
+        if boundary_masks is not None:
+            forground_masks = (x != 0) & ~boundary_masks
+        else:
+            forground_masks = (x != 0)
+
+        return forground_masks
