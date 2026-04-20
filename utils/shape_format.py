51a4d969c0c5509aa6270bd80195a3da53237fe9
panzax <martin.alvarez.kuglen@gmail.com>
2026-04-20 15:07:31 -0700

    Prevent mutation during spatial shape extraction

--- utils/shape_format.py
+++ utils/shape_format.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 
 def get_spatial_shape(shape: tuple, fmt: str) -> tuple:
     axis_to_value = OrderedDict[str, int](zip(fmt, shape))
-    for key, value in axis_to_value.items():
+    for key, value in axis_to_value.copy().items():
         if key.upper() not in ["Z", "Y", "X"]:
             axis_to_value.pop(key)
     return tuple(axis_to_value.values())
